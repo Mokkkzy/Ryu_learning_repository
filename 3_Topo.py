d5c92e49e060cbe82457f9dfe44769cabed70486
Mok <mok@MokdeMacBook-Air.local>
2022-02-28 22:04:46 +0800

    22/02/28

--- 3_Topo.py
+++ 3_Topo.py
@@ -7,23 +7,28 @@ from mininet.log import setLogLevel
 class ECMP_round_request(Topo):
     "ECMP_RR_Test is processing."
     def __init__(self):
-        switch = []
-        host = []
-        for s in range(6):
-            switch[s+1] = self.addSwitch('s%s'%(s+1))
+        switch1 = self.addSwitch('s1')
+        switch2 = self.addSwitch('s2')
+        switch3 = self.addSwitch('s3')
+        switch4 = self.addSwitch('s4')
+        switch5 = self.addSwitch('s5')
+        switch6 = self.addSwitch('s6')
         
-        for h in range(4):
-            host[h+1] = self.addHost('h%h'%(h+1))
+        host1 = self.addHost('h1')
+        host2 = self.addHost('h2')
+        host3 = self.addHost('h3')
+        host4 = self.addHost('h4')
         
-        self.addLink(host[1],switch[3])
-        self.addLink(host[2],switch[3])
-        self.addLink(switch[3],switch[1])
-        self.addLink(switch[1],switch[5])
-        self.addLink(switch[1],switch[6])
-        self.addLink(switch[5],switch[2])
-        self.addLink(switch[6],switch[2])
-        self.addLink(host[3],switch[2])
-        self.addLink(host[4],switch[2])
+        
+        self.addLink(host1,switch3)
+        self.addLink(host2,switch3)
+        self.addLink(switch3,switch1)
+        self.addLink(switch1,switch5)
+        self.addLink(switch1,switch6)
+        self.addLink(switch5,switch2)
+        self.addLink(switch6,switch2)
+        self.addLink(host3,switch2)
+        self.addLink(host4,switch2)
         
 topos = {"mytopo":(lambda:ECMP_round_request())}
     
