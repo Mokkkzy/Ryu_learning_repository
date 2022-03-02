f41dba97dfaee4f9cb904911c2f22f2b0b01a60b
Mok <mok@MokdeMacBook-Air.local>
2022-03-02 10:52:05 +0800

    22/03/02

--- 3_Topo.py
+++ 3_Topo.py
@@ -8,25 +8,18 @@ class MyTopo (Topo):
         switch2 = self.addSwitch('s2')
         switch3 = self.addSwitch('s3')
         switch4 = self.addSwitch('s4')
-        switch5 = self.addSwitch('s5')
-        switch6 = self.addSwitch('s6')
     
         host1 = self.addHost('h1')
         host2 = self.addHost('h2')
-        host3 = self.addHost('h3')
-        host4 = self.addHost('h4')
         
         
         self.addLink(host1,switch3)
         self.addLink(host2,switch3)
         self.addLink(switch3,switch1)
-        self.addLink(switch1,switch5)
-        self.addLink(switch1,switch6)
-        self.addLink(switch5,switch2)
-        self.addLink(switch6,switch2)
+        self.addLink(switch1,switch4)
+        self.addLink(switch3,switch2)
         self.addLink(switch2,switch4)
-        self.addLink(host3,switch4)
-        self.addLink(host4,switch4)
+
         
 topos = {"mytopo":(lambda:MyTopo())}
     
